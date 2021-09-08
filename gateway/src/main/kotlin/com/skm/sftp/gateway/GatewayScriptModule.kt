ce24de7f49846579b31cf9aec17dbfd4b9336823
Scott Key <scottdkey@gmail.com>
2021-09-08 15:07:02 -0600

    JAR SIZE IS BIGGER AND INCLUDES EVERYTHING

--- gateway/src/main/kotlin/com/skm/sftp/gateway/GatewayScriptModule.kt
+++ gateway/src/main/kotlin/com/skm/sftp/gateway/GatewayScriptModule.kt
@@ -1,12 +1,16 @@
 package com.skm.sftp.gateway
 
+import com.jcraft.jsch.*
 import com.skm.sftp.AbstractScriptModule
 import net.schmizz.sshj.SSHClient
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier
+import org.slf4j.LoggerFactory
 import java.io.IOException
 
 
 class GatewayScriptModule : AbstractScriptModule() {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @Throws(IOException::class)
     private fun setupSshj(username:String, password: String, remoteHost: String): SSHClient {
         val client = SSHClient()
@@ -15,7 +19,18 @@ class GatewayScriptModule : AbstractScriptModule() {
         client.authPassword(username, password)
         return client
     }
-    override fun uploadImpl(
+    @Throws(JSchException::class)
+    private fun setupJsch(username: String, password: String, remoteHost: String): ChannelSftp {
+        val jsch = JSch()
+        val jschSession = jsch.getSession(username, remoteHost)
+        val config = java.util.Properties()
+        config["StrictHostKeyChecking"] = "no"
+        jschSession.setConfig(config)
+        jschSession.setPassword(password)
+        jschSession.connect()
+        return jschSession.openChannel("sftp") as ChannelSftp
+    }
+    private fun altUpload(
         remoteHost: String,
         username: String,
         password: String,
@@ -28,8 +43,36 @@ class GatewayScriptModule : AbstractScriptModule() {
         sftpClient?.close()
         sshClient.disconnect()
     }
+    override fun uploadImpl(
+        remoteHost: String,
+        username: String,
+        password: String,
+        localPath: String,
+        remotePath: String
+    ) {
+    var channelSftp: ChannelSftp? = null
+        try {
+            channelSftp = setupJsch(username, password, remoteHost)
+        } catch (e: JSchException){
+            logger.info("jsch error", e)
+        }
+        try {
+            channelSftp?.connect()
+        } catch(e: JSchException){
+            logger.info("jsch unable to connect", e)
+            logger.info("alt attempt", "trying sshj library")
+            altUpload(remoteHost, username, password, localPath, remotePath)
+        }
+        try {
+            channelSftp?.put(localPath, remotePath)
+            println("Upload Complete")
+        }catch (e: SftpException){
+            logger.info("sftp upload failed", e)
+        }
 
-    override fun downloadImpl(
+    }
+
+    private fun altDownload(
         remoteHost: String,
         username: String,
         password: String,
@@ -41,5 +84,34 @@ class GatewayScriptModule : AbstractScriptModule() {
         sftpClient?.get(remotePath, localPath)
         sftpClient?.close()
         sshClient.disconnect()
+    }
+
+    override fun downloadImpl(
+        remoteHost: String,
+        username: String,
+        password: String,
+        localPath: String,
+        remotePath: String
+    ) {
+        var channelSftp: ChannelSftp? = null
+        try {
+            channelSftp = setupJsch(username, password, remoteHost)
+        } catch (e: JSchException){
+            logger.info("jsch error", e)
+
+        }
+        try {
+            channelSftp?.connect()
+        } catch(e: JSchException){
+            logger.info("jsch unable to connect", e)
+            logger.info("alt attempt", "trying sshj library")
+            altDownload(remoteHost, username, password, localPath, remotePath)
+        }
+        try {
+            channelSftp?.get(localPath, remotePath)
+            println("Upload Complete")
+        }catch (e: SftpException){
+            logger.info("sftp upload failed", e)
+        }
     }
 }
